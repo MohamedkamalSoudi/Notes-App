0fb8913bbacef502c098b74c0db47d0bc9c6fae2
MohamedkamalSoudi <mk0875734@gmail.com>
2024-07-27 17:12:30 +0300

    init hive and open notes box

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/widgets/constants.dart';
 import 'package:notes_app/widgets/notes_view.dart';
 
-void main() {
+void main() async{
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(const NotesApp());
 }
 
@@ -14,7 +19,7 @@ class NotesApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false, 
       theme: ThemeData(brightness: Brightness.dark,fontFamily: 'Poppins'),
-      home:const NotesView(),
+      home:const NotesView(), 
     );
     
   }
--- lib/widgets/constants.dart
+++ lib/widgets/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor =  Colors.purple;
+const kPrimaryColor = Colors.purple;
+const kNotesBox = 'notes_box';
