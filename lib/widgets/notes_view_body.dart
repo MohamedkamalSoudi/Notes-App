40ff8ebf3ccef86846656f28d98c08a39a20fa6e
MohamedkamalSoudi <mk0875734@gmail.com>
2024-07-30 20:24:22 +0300

    display notes part one

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,32 +1,44 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits_add_note_cubit/notes_cubit.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 import 'package:notes_app/widgets/notes_list_view.dart';
 
-class NotesViewBody extends StatelessWidget {
+class NotesViewBody extends StatefulWidget {
   const NotesViewBody({super.key});
 
   @override
+  State<NotesViewBody> createState() => _NotesViewBodyState();
+}
+
+class _NotesViewBodyState extends State<NotesViewBody> {
+  void initState() {
+    BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(
+    return  Padding(
+      padding:const EdgeInsets.symmetric(
         horizontal: 24,
       ),
       child: Column(
-        children: const [
+        children:const[
           SizedBox(
             height: 50,
           ),
           CustomAppBar(
-            title:'Notes',
+            title: 'Notes',
             icon: Icons.search,
           ),
-        Expanded(child: NotesListView()),
+          Expanded(
+            child: NotesListView()
+            ),
         ],
       ),
     );
   }
 }
-
-
